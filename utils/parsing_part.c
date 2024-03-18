8f7c8638e3062b7d985302bc17490dd2e33d301c
Mehdi Serghini <meserghi@e3r6p4.1337.ma>
2024-03-18 16:58:55 +0000

    done ...

--- utils/parsing_part.c
+++ utils/parsing_part.c
@@ -6,7 +6,7 @@
 /*   By: meserghi <meserghi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 03:41:37 by meserghi          #+#    #+#             */
-/*   Updated: 2024/03/18 03:46:01 by meserghi         ###   ########.fr       */
+/*   Updated: 2024/03/18 16:55:59 by meserghi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,42 +24,55 @@ int	check_token(char c)
 		return (t_red_out);
 	else if (c == '|')
 		return (t_pipe);
+	else if (c == '\0')
+		return (t_last);
 	return (-1);
 }
 
 void	add_to_list(t_list **head, char *input, int s, int pos)
 {
-	char	*res;
-	char	*res_cln;
+    char	*res;
+    char	*res_cln;
 
-	res = ft_substr(input, s, pos);
-	res_cln = ft_strtrim(res, " \t");
-	add_back(head, new_node(res_cln, t_word));
+    res = ft_substr(input, s, pos);
+    res_cln = ft_strtrim(res, " \t");
+    add_back(head, new_node(res_cln, t_word));
+    if (input[s + pos] != ' ' && input[s + pos] != '\0') {
+        res = ft_substr(input, s + pos, 1);
+        add_back(head, new_node(res, check_token(input[s + pos])));
+    }
 }
 
 t_list	*tokening(char *input)
 {
-	int		i;
-	int		s;
-	int		len;
-	t_list	*head;
+    int		i;
+    int		s;
+    int		len;
+    t_list	*head;
 
-	i = 0;
-	s = 0;
-	head = NULL;
-	len = ft_strlen(input);
-	if (!input || !*input)
-		return (NULL);
-	while (i < len)
-	{
-		if (check_token(input[i]) != -1)
-		{
-			add_to_list(&head, input, s, i - s);
-			s = i + 1;
-		}
-		i++;
-	}
-	return (head);
+    i = 0;
+    s = 0;
+    head = NULL;
+    len = ft_strlen(input);
+    if (!input || !*input)
+        return (NULL);
+    while (i <= len)
+    {
+        if (check_token(input[i]) != -1)
+        {
+            char *res = ft_substr(input, s, i - s);
+            char *res_cln = ft_strtrim(res, " \t");
+			if (*res_cln)
+            add_back(&head, new_node(res_cln, t_word));
+            if (input[i] != '\0') {
+                char *res = ft_substr(input, i, 1);
+                add_back(&head, new_node(res, check_token(input[i])));
+            }
+            s = i + 1;
+        }
+        i++;
+    }
+    return (head);
 }
 
 t_list	*parsing_part(char *line)
